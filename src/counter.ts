803cbd299b49e7beacd0d87c47f78847d77e7d7c
Jorge Castro <david92_jc@yahoo.es>
2023-11-10 01:00:22 -0500

    Correcciones menores

--- src/counter.ts
+++ src/counter.ts
@@ -1,11 +1,31 @@
 import $ from "jquery";
+import typescriptLogo from './typescript.svg'
+import viteLogo from '/vite.svg'
 
-export function setupCounter(element: JQuery) {
+export function setupCounter() {
   let counter = 0
   const setCounter = (count: number) => {
     counter = count
-    $(element).html(`count is ${counter}`)
+    $('#counter').html(`count is ${counter}`)
   }
-  element.on('click', () => setCounter(counter + 1))
+  $('#counter').on('click', () => setCounter(counter + 1))
   setCounter(0)
 }
+
+export const Counter = /*html*/`
+    <div>
+      <a href="https://vitejs.dev" target="_blank">
+        <img src="${viteLogo}" class="logo" alt="Vite logo" />
+      </a>
+      <a href="https://www.typescriptlang.org/" target="_blank">
+        <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
+      </a>
+      <h1>Vite + TypeScript</h1>
+      <div class="card">
+        <button id="counter" type="button"></button>
+      </div>
+      <p class="read-the-docs">
+        Click on the Vite and TypeScript logos to learn more
+      </p>
+    </div>
+  `
